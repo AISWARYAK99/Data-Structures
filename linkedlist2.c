fcd012ec2418ba19f1fee3674769afcf2aaa273a
AISWARYA K <66054986+AISWARYAK99@users.noreply.github.com>
2020-11-08 20:10:36 +0530

    linked list
    Creating single linked list from an array.

---
+++ linkedlist2.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next;
+}*first=NULL;
+
+void create(int A[],int n)
+{
+    int i;
+    struct Node *t,*last;
+    first=(struct Node *)malloc(sizeof(struct Node));
+    first->data=A[0];
+    last=first;
+    for(i=1;i<n;i++)
+    {
+        t=(struct Node *)malloc(sizeof(struct Node));
+        t->data=A[i];
+        t->next=NULL;
+        last->next=t;
+        last=t;
+    }
+}
+void display(struct Node *p)
+{
+    while(p!=NULL)
+    {
+        printf("%d\t",p->data);
+        p=p->next;
+    }
+}
+int main()
+{
+    int A[10]={3,5,8,9,60};
+    create(A,5);
+    display(first);
+
+}
