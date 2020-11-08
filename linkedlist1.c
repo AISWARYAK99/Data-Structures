0fd0c85eec92a18cd6590d32a91935cc089202c6
AISWARYA K <66054986+AISWARYAK99@users.noreply.github.com>
2020-11-08 20:09:00 +0530

    Add files via upload
    Simple linked list of 2 nodes.

---
+++ linkedlist1.c
@@ -0,0 +1,29 @@
+/* creating linked list of 2 nodes*/
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+void display(struct Node *p)
+{
+    while(p!=NULL)
+    {
+        printf("%d\t",p->data);
+        p=p->next;
+    }
+}
+int main()
+{
+    struct Node *first,*current;
+    first=(struct Node *)malloc(sizeof(struct Node));
+    first->data=10;
+    first->next=NULL;
+    current =(struct Node *)malloc(sizeof(struct Node));
+    current->data=100;
+    current->next=NULL;
+    first->next=current;
+    display(first);
+
+}
